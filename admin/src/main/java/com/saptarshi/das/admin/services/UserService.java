4b6b6d375a967b3c3adb7cbdebf148dbb797a742
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-02 16:42:37 +0530

    Update UserService to use UserRepository
    Updated UserService to use UserRepository to fetch data instead of returning static data.

--- admin/src/main/java/com/saptarshi/das/admin/services/UserService.java
+++ admin/src/main/java/com/saptarshi/das/admin/services/UserService.java
@@ -1,23 +1,17 @@
 package com.saptarshi.das.admin.services;
 
 import com.saptarshi.das.admin.models.User;
+import com.saptarshi.das.admin.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
     public List<User> getUsers() {
-        final List<User> users = new ArrayList<>();
-        users.add(User.builder().id(UUID.randomUUID().toString()).username("saptarshi.das").firstName("Saptarshi").lastName("Das").email("saptarshi.das@mail.com").build());
-        users.add(User.builder().id(UUID.randomUUID().toString()).username("debjit.ghosh").firstName("Debjit").lastName("Ghosh").email("debjit.ghosh@mail.com").build());
-        users.add(User.builder().id(UUID.randomUUID().toString()).username("abhiroop.das").firstName("Abhiroop").lastName("Das").email("abhiroop.das@mail.com").build());
-        users.add(User.builder().id(UUID.randomUUID().toString()).username("ritam.biswas").firstName("Ritam").lastName("Biswas").email("ritam.biswas@mail.com").build());
-        users.add(User.builder().id(UUID.randomUUID().toString()).username("soumyadeep.manna").firstName("Soumyadeep").lastName("Manna").email("soumyadeep.manna@mail.com").build());
-        users.add(User.builder().id(UUID.randomUUID().toString()).username("debdutta.chatterjee").firstName("Debdutta").lastName("Chatterjee").email("debdutta.chatterjee@mail.com").build());
-
-        return users;
+        return userRepository.findAll();
     }
 }
