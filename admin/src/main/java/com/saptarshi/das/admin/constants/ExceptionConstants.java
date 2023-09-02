e95856e887cd6126429f5216e2d5bc012512ee75
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-02 22:15:05 +0530

    Update ExceptionConstants
    Updated ExceptionConstants to add new error message.

--- admin/src/main/java/com/saptarshi/das/admin/constants/ExceptionConstants.java
+++ admin/src/main/java/com/saptarshi/das/admin/constants/ExceptionConstants.java
@@ -3,4 +3,5 @@ package com.saptarshi.das.admin.constants;
 public class ExceptionConstants {
     public static final String SOME_WENT_WRONG_MESSAGE = "Oops!!! Something went wrong. Please try again.";
     public static final String FORBIDDEN_USER_MESSAGE = "Invalid Email or Password.";
+    public static final String ACCESS_DENIED_MESSAGE = "Access Denied.";
 }
