41a69189f8937bf62e82a6dea467ba6c18cf7e12
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-02 22:15:55 +0530

    Add Authorization on test controller method
    Added sample authorization rule on test controller method.

--- admin/src/main/java/com/saptarshi/das/admin/controllers/TestController.java
+++ admin/src/main/java/com/saptarshi/das/admin/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.saptarshi.das.admin.controllers;
 import com.saptarshi.das.admin.models.User;
 import com.saptarshi.das.admin.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class TestController {
     private UserService userService;
 
     @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getUsers() {
         return userService.getUsers();
     }
--- admin/src/main/java/com/saptarshi/das/admin/services/AuthenticationService.java
+++ admin/src/main/java/com/saptarshi/das/admin/services/AuthenticationService.java
@@ -36,7 +36,7 @@ public class AuthenticationService {
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
+                .role(Role.ROLE_ADMIN)
                 .build();
 
         userRepository.save(user);
