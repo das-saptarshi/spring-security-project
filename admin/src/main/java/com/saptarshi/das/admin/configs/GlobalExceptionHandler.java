37480c2adffff905e03dac41db9f94bbae2e5436
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-02 22:14:17 +0530

    Update GlobalExceptionHandler
    Updated GlobalExceptionHandler to remove Exception class and add AccessDeniedException class handlers.

--- admin/src/main/java/com/saptarshi/das/admin/configs/GlobalExceptionHandler.java
+++ admin/src/main/java/com/saptarshi/das/admin/configs/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.saptarshi.das.admin.exceptions.BaseException;
 import com.saptarshi.das.admin.responses.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,20 +16,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 @Component
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler({ Exception.class })
-    public ResponseEntity<ExceptionResponse> handleOtherException(
-            final Exception ex,
-            final WebRequest request
-            ) {
-        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
-                .message(ExceptionConstants.SOME_WENT_WRONG_MESSAGE)
-                .build();
-        return new ResponseEntity<>(
-                exceptionResponse,
-                HttpStatus.BAD_REQUEST
-        );
-    }
 
     @ExceptionHandler({ BaseException.class })
     public ResponseEntity<ExceptionResponse> handleBaseException(
@@ -50,6 +37,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     ) {
         final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
                 .message(ExceptionConstants.FORBIDDEN_USER_MESSAGE)
+                .build();
+        return new ResponseEntity<>(
+                exceptionResponse,
+                HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler({ AccessDeniedException.class })
+    public ResponseEntity<ExceptionResponse> handleAccessDeniedException(
+            final AccessDeniedException ex,
+            final WebRequest request
+    ) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .message(ExceptionConstants.ACCESS_DENIED_MESSAGE)
                 .build();
         return new ResponseEntity<>(
                 exceptionResponse,
