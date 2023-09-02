2240b5a0ce84878f08fb3a90236fce6444765627
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-02 16:46:08 +0530

    Add request and response classes for authentication
    Added request and response classes needed for authentication process.

---
+++ admin/src/main/java/com/saptarshi/das/admin/requests/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.saptarshi.das.admin.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
---
+++ admin/src/main/java/com/saptarshi/das/admin/services/RegistrationResponse.java
@@ -0,0 +1,14 @@
+package com.saptarshi.das.admin.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationResponse {
+    private String message;
+}
