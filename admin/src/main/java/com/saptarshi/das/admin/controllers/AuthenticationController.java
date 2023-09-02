2ac5a61464c6a649a370d86b508f2ecc09230d55
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-02 16:48:42 +0530

    Add Authentication controller and service
    Added Authentication controller and service with two endpoints - register and authenticate.
    /register: To register a new user.
    /authenticate: To authenticate email and password of a user and generate JWT token on success.

---
+++ admin/src/main/java/com/saptarshi/das/admin/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.saptarshi.das.admin.controllers;
+
+import com.saptarshi.das.admin.exceptions.UserAlreadyExistsException;
+import com.saptarshi.das.admin.requests.AuthenticationRequest;
+import com.saptarshi.das.admin.requests.RegisterRequest;
+import com.saptarshi.das.admin.responses.AuthenticationResponse;
+import com.saptarshi.das.admin.services.AuthenticationService;
+import com.saptarshi.das.admin.services.RegistrationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+    @PostMapping("/register")
+    public RegistrationResponse register(
+            @RequestBody RegisterRequest request
+    ) throws UserAlreadyExistsException {
+        return authenticationService.register(request);
+    }
+
+    @PostMapping("/authenticate")
+    public AuthenticationResponse authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return authenticationService.authenticate(request);
+    }
+}
---
+++ admin/src/main/java/com/saptarshi/das/admin/services/AuthenticationService.java
@@ -0,0 +1,66 @@
+package com.saptarshi.das.admin.services;
+
+import com.saptarshi.das.admin.exceptions.UserAlreadyExistsException;
+import com.saptarshi.das.admin.models.Role;
+import com.saptarshi.das.admin.models.User;
+import com.saptarshi.das.admin.repositories.UserRepository;
+import com.saptarshi.das.admin.requests.AuthenticationRequest;
+import com.saptarshi.das.admin.requests.RegisterRequest;
+import com.saptarshi.das.admin.responses.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public RegistrationResponse register(final RegisterRequest request) throws UserAlreadyExistsException {
+        final User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        if (userRepository.exists(Example.of(user))) {
+            throw new UserAlreadyExistsException("User Already Exists!");
+        }
+
+        userRepository.save(user);
+
+        return RegistrationResponse.builder()
+                .message("User Registered Successfully.")
+                .build();
+    }
+
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        final UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+        );
+
+        authenticationManager.authenticate(authToken);
+        final Optional<User> user = userRepository.findByEmail(request.getEmail());
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        final String token = jwtService.generateToken(user.get());
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+}
