d683947e532e8c2686c1fb0b1a831c879b8d2bab
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-03 15:38:38 +0530

    Add new DemoService module
    Added new module DemoService which will be the service for client APIs.

---
+++ demoservice/src/test/java/com/saptarshi/das/demoservice/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saptarshi.das.demoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
