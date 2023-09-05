b12db4666f3494397f112b796f48dbfb02bd5c1b
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-05 23:57:59 +0530

    Fix User and Admin annotations
    Fixed Admin and User annotations.

--- demoservice/src/main/java/com/saptarshi/das/demoservice/annotations/IsAdmin.java
+++ demoservice/src/main/java/com/saptarshi/das/demoservice/annotations/IsAdmin.java
@@ -2,6 +2,13 @@ package com.saptarshi.das.demoservice.annotations;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public @interface IsAdmin {
 }
